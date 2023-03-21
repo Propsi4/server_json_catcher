76f1acd466661a2cc0d5775f609ff8a939ab7992
BuildTools <unconfigured@null.spigotmc.org>
2023-03-21 23:56:04 +0200

    fast fix

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 const PORT = 5000;
-
+let index = 1
 const app = express();
 app.use(express.json());
 app.post('/api/get', (req, res) => {
+    console.log(`=================${index}===================`)
+    try{
     res.status(200).send('Got it!')
     console.log(req.body)
+    index++
+    } catch (err) {}
   })
 
 app.listen(PORT, () => {
